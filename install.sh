9bf2ed53ca61c8918cf7864408922b24640b456e
hanshenu <hanshenu@gmail.com>
2016-01-17 22:12:59 +0800

    add tmux conf

--- install.sh
+++ install.sh
@@ -8,22 +8,16 @@ cd ~/.vim
 
 echo "create .vimrc"
 rm ~/.vimrc
-rm ~/.gvimrc
 ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/vimrc ~/.gvimrc
-rm ~/.gitconfig
 
-ln -s ~/.vim/.gitconfig ~/.gitconfig
+rm ~/.tmux.conf
+ln -s ~/.vim/tmux.conf ~/.tmux.conf
 
-
+#rm ~/.gitconfig
+#ln -s ~/.vim/.gitconfig ~/.gitconfig
 
 
 echo "update submodules"
 git submodule init
 git submodule update
 
-echo "remove lua-ftplugin to prevent doc error msg"
-
-rm bundle/vim-lua-ftplugin/doc/lua-ftplugin.txt
-
-git submodule foreach git pull origin master
